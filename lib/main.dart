b2ed987f235779cb1d8915a358fd4c88c95ac26a
satoshii <webmaster@satoshii.org>
2023-02-07 21:35:55 +0900

    pickColor内でタップ〜ドラッグのイベントのたびにデコード等が走るのを修正

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,22 @@ class _MyHomePageState extends State<MyHomePage> {
   final _imageBytes = ValueNotifier<Uint8List?>(null);
   /// 選択された座標と色
   final _offsetColor = ValueNotifier<OffsetColor>(OffsetColor(null, null));
+  // このあたりはごちゃごちゃするが pickColor() 内で同じ処理が複数回走らないようプロパティにする
+  /// 画像の表示領域の横幅
+  late double _imageAreWidth;
+  /// 画像の表示領域の縦幅
+  late double _imageAreHeight;
+  /// 画像のimg.Image表現
+  late img.Image _imgImage;
+  /// 画像の縮小率
+  late double _imageRatio;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _imageAreWidth = MediaQuery.of(context).size.width * imageAreaWidthRatio;
+    _imageAreHeight = MediaQuery.of(context).size.height * imageAreaHeightRatio;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             // _offsetColorを監視して再描画する
             ValueListenableBuilder(
               valueListenable: _offsetColor,
@@ -70,9 +86,8 @@ class _MyHomePageState extends State<MyHomePage> {
             // 画像表示領域
             Container(
               alignment: Alignment.center,
-              // TODO 画像表示領域のサイズ設定はもうちょっと考える
-              width: MediaQuery.of(context).size.width * imageAreaWidthRatio,
-              height: MediaQuery.of(context).size.height * imageAreaHeightRatio,
+              width: _imageAreWidth,
+              height: _imageAreHeight,
               child: Stack(
                 children: [
                   // 選択された画像を描画する
@@ -85,8 +100,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       }
                       // 画像を表示してタップ時の挙動を設定
                       return GestureDetector(
-                        // TODO onPanUpdate にしてなめらかに取れるようにする？
-                        onTapDown: pickColor,
+                        onPanStart: pickColor,
+                        onPanUpdate: pickColor,
                         child: Image.memory(imageBytes),
                       );
                     },
@@ -122,36 +137,35 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  /// カメラロールから画像を選択しimageBytesにセット
+  /// カメラロールから画像を選択し _imageBytes と _imgImage にセット
+  /// 同時に _imageRatio もセットする
   void selectImage() async {
     final ImagePicker picker = ImagePicker();
     final XFile? image = await picker.pickImage(source: ImageSource.gallery);
     if(image == null) {
       return;
     }
-    _imageBytes.value = await image.readAsBytes();
+    Uint8List bytes = await image.readAsBytes();
+    // 一応未知のエンコード形式ではnullを返すと思われるがエラー処理は省略
+    _imgImage = img.decodeImage(bytes)!;
+
+    // 画像サイズが表示領域のサイズより大きい場合の縮小率
+    double widthRatio = _imageAreWidth < _imgImage.width ? (_imageAreWidth / _imgImage.width) : 1;
+    double heightRatio = _imageAreHeight < _imgImage.height ? (_imageAreHeight / _imgImage.height) : 1;
+    _imageRatio = min(widthRatio, heightRatio);
+
+    _imageBytes.value = bytes;
   }
 
   /// TapDownDetailsで指定された座標と色をoffsetColorにセットする
-  void pickColor(TapDownDetails details) {
-    // 一応未知のエンコード形式ではnullを返すと思われるがエラー処理は省略
-    img.Image image = img.decodeImage(_imageBytes.value!)!;
-
-    // 画像の表示領域のサイズ
-    double width = MediaQuery.of(context).size.width * imageAreaWidthRatio;
-    double height = MediaQuery.of(context).size.height * imageAreaHeightRatio;
-
-    // 画像サイズが表示領域のサイズより大きい場合の縮小率
-    double widthRatio = width < image.width ? (width / image.width) : 1;
-    double heightRatio = height < image.height ? (height / image.height) : 1;
-    double ratio = min(widthRatio, heightRatio);
-
+  /// 引数のdetailsはGestureDragStartCallbackまたはGestureDragUpdateCallback
+  void pickColor(details) {
     // タップ位置を画像の対応する位置に変換
-    double x =  details.localPosition.dx / ratio;
-    double y =  details.localPosition.dy / ratio;
+    int x =  details.localPosition.dx ~/ _imageRatio;
+    int y =  details.localPosition.dy ~/ _imageRatio;
 
     // 座標と色を取得してセット
-    img.Pixel pixel = image.getPixel(x.toInt(), y.toInt());
+    img.Pixel pixel = _imgImage.getPixel(x, y);
     Color color = Color.fromARGB(pixel.a.toInt(), pixel.r.toInt(), pixel.g.toInt(), pixel.b.toInt());
     // Offsetはイミュータブルなのでコピーする必要はない
     Offset offset = details.localPosition;
