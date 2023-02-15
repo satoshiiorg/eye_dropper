da2c8bf16f28a8b2d4751a46b1f63f17d536f1e4
satoshii <webmaster@satoshii.org>
2023-02-15 23:36:24 +0900

    lintに沿って修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,30 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
-import 'dart:math';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// 本当はStateNotifierProviderを使うべきだが手抜き
 /// 画像表示領域のサイズ
-final imageAreaSizeProvider = Provider<Size>((ref) => const Size(0, 0));
+final imageAreaSizeProvider = Provider<Size>((ref) => Size.zero);
+// 本当はStateNotifierProviderを使うべきだが手抜き
 /// 画像関連の情報
 final imageProvider = StateProvider<MyImage?>((ref) => null);
 /// 選択された座標と色
 final offsetColorProvider = StateProvider<OffsetColor>(
-        (ref) => OffsetColor(null, null));
+        (ref) => OffsetColor(null, null),);
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
   /// 画像の表示領域の画面サイズに対する比率(横)
   static const imageAreaWidthRatio = 0.95;
   /// 画像の表示領域の画面サイズに対する比率(縦)
   static const imageAreaHeightRatio = 0.65;
-
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -35,17 +35,18 @@ class MyApp extends ConsumerWidget {
       ),
       home: Builder(builder: (context) {
         // 画像表示領域のサイズを設定
-        Size screenSize = MediaQuery.of(context).size;
-        Size imageAreaSize = Size(
+        final screenSize = MediaQuery.of(context).size;
+        final imageAreaSize = Size(
             screenSize.width * imageAreaWidthRatio,
-            screenSize.height * imageAreaHeightRatio);
+            screenSize.height * imageAreaHeightRatio,
+        );
         return ProviderScope(
           overrides: [
             imageAreaSizeProvider.overrideWith((ref) => imageAreaSize),
           ],
           child: const MyHomePage(title: 'スポイトツール'),
         );
-      }),
+      },),
     );
   }
 }
@@ -57,9 +58,9 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Size imageAreaSize = ref.watch(imageAreaSizeProvider);
-    MyImage? image = ref.watch(imageProvider);
-    OffsetColor offsetColor = ref.watch(offsetColorProvider);
+    final imageAreaSize = ref.watch(imageAreaSizeProvider);
+    final image = ref.watch(imageProvider);
+    final offsetColor = ref.watch(offsetColorProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -121,32 +122,33 @@ class MyHomePage extends ConsumerWidget {
   }
 
   /// カメラロールから画像を選択し imageProvider にセット
-  void selectImage(WidgetRef ref) async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+  Future<void> selectImage(WidgetRef ref) async {
+    final picker = ImagePicker();
+    final image = await picker.pickImage(source: ImageSource.gallery);
     if(image == null) {
       return;
     }
-    Uint8List bytes = await image.readAsBytes();
-    Size imageAreaSize = ref.read(imageAreaSizeProvider)!;
+    final bytes = await image.readAsBytes();
+    final imageAreaSize = ref.read(imageAreaSizeProvider);
     ref.read(imageProvider.notifier).state = MyImage(bytes, imageAreaSize);
   }
 
   /// TapDownDetailsで指定された座標と色をoffsetColorProviderにセットする
   void pickColor(Offset localPosition, WidgetRef ref) {
-    MyImage image = ref.watch(imageProvider)!;
+    final image = ref.watch(imageProvider)!;
     // タップ位置を画像の対応する位置に変換
-    double dx = localPosition.dx / image.ratio;
-    double dy = localPosition.dy / image.ratio;
+    final dx = localPosition.dx / image.ratio;
+    final dy = localPosition.dy / image.ratio;
 
     // 座標と色を取得してセット
-    img.Pixel pixel = image.imgImage.getPixelSafe(dx.toInt(), dy.toInt());
+    final pixel = image.imgImage.getPixelSafe(dx.toInt(), dy.toInt());
     // ドラッグしたまま画像の範囲外に行くとRangeErrorになるので対策
     if(pixel == img.Pixel.undefined) {
       return;
     }
-    Color color = Color.fromARGB(
-        pixel.a.toInt(), pixel.r.toInt(), pixel.g.toInt(), pixel.b.toInt());
+    final color = Color.fromARGB(
+        pixel.a.toInt(), pixel.r.toInt(), pixel.g.toInt(), pixel.b.toInt(),
+    );
     // localPositionはイミュータブルなのでそのまま渡してよい
     ref.read(offsetColorProvider.notifier).state =
         OffsetColor(localPosition, color);
@@ -155,43 +157,45 @@ class MyHomePage extends ConsumerWidget {
 
 /// 画像関連のデータ
 class MyImage {
+  // 一応未知のエンコード形式ではnullを返すと思われるがエラー処理は省略
+  MyImage(this.bytes, Size imageAreaSize) : imgImage = img.decodeImage(bytes)! {
+    final widthRatio = imageAreaSize.width < imgImage.width ?
+                      (imageAreaSize.width / imgImage.width) : 1.0;
+    final heightRatio = imageAreaSize.height < imgImage.height ?
+                      (imageAreaSize.height / imgImage.height) : 1.0;
+    ratio = min(widthRatio, heightRatio);
+  }
+
   /// 画像のバイト列表現
   final Uint8List bytes;
   /// 画像のimg.Image表現
   final img.Image imgImage;
   /// 画像の縮小率
   late final double ratio;
-
-  // 一応未知のエンコード形式ではnullを返すと思われるがエラー処理は省略
-  MyImage(this.bytes, imageAreaSize) : imgImage = img.decodeImage(bytes)! {
-    double widthRatio = imageAreaSize.width < imgImage.width ?
-                      (imageAreaSize.width / imgImage.width) : 1;
-    double heightRatio = imageAreaSize.height < imgImage.height ?
-                      (imageAreaSize.height / imgImage.height) : 1;
-    ratio = min(widthRatio, heightRatio);
-  }
 }
 
 /// 座標と色のペア
 class OffsetColor {
+  OffsetColor(this.offset, this.color);
+
   final Offset? offset;
   final Color? color;
-  OffsetColor(this.offset, this.color);
 }
 
 /// 吸い取った色の表示領域
 class PickedPainter extends CustomPainter {
+  PickedPainter(this.color);
+
   static const double rectSize = 50;
   Color? color;
-  PickedPainter(this.color);
 
   @override
   void paint(Canvas canvas, Size size) {
     // 選択された色で塗りつぶした四角を表示
-    Paint p = Paint();
-    p.color = color ?? Colors.white;
-    p.style = PaintingStyle.fill;
-    Rect r = const Rect.fromLTWH(0, 0, rectSize, rectSize);
+    final p = Paint()
+              ..color = color ?? Colors.white
+              ..style = PaintingStyle.fill;
+    const r = Rect.fromLTWH(0, 0, rectSize, rectSize);
     canvas.drawRect(r, p);
   }
 
@@ -210,11 +214,11 @@ class TapPointPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     // 赤い四角で囲う
-    Paint p = Paint();
-    p.color = Colors.red;
-    p.style = PaintingStyle.stroke;
-    p.strokeWidth = strokeWidth;
-    Rect r = const Rect.fromLTWH(0, 0, rectSize, rectSize);
+    final p = Paint()
+            ..color = Colors.red
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = strokeWidth;
+    const r = Rect.fromLTWH(0, 0, rectSize, rectSize);
     canvas.drawRect(r, p);
   }
 
